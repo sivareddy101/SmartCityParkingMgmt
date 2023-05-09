11ed451d52373ae162fce7bf3d6d33456742c4d2
sivareddy101 <132719318+sivareddy101@users.noreply.github.com>
2023-05-09 16:39:30 -0400

    Update test.py

--- test.py
+++ test.py
@@ -10,7 +10,6 @@ import seaborn as sns
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error, mean_absolute_error
-from sklearn.ensemble import RandomForestRegressor
 from sklearn.svm import SVC
 from sklearn.tree import DecisionTreeRegressor
 from sklearn.metrics import accuracy_score
