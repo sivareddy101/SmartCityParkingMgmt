0a6e7e445153272279a7466bf0542f8339c47366
sivareddy101 <132719318+sivareddy101@users.noreply.github.com>
2023-05-09 13:20:54 -0400

    Update test.py

--- test.py
+++ test.py
@@ -1,5 +1,13 @@
+##Required Libraries
 import streamlit as st 
+import pandas as pd
 
 
-st.title("Testing!!")
-st.write("This is just for testing!!!")
+st.title("👋 Welcome to Smart Parking Manamgement App v1.0")
+#Starting from the top
+st.markdown("# Dedicated for Smart City Management Framework™")
+st.markdown("Project realized by: Naga Srinivas Reddy & Naga Siva Reddy & Shravan Yadav")
+original_title = '<p style="color:Orange; font-size: 30px;">/p>'
+st.markdown(original_title, unsafe_allow_html=True)
+
+
