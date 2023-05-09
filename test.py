bf8f633d079ea780d79d06695589b598398594fc
sivareddy101 <132719318+sivareddy101@users.noreply.github.com>
2023-05-09 13:25:19 -0400

    Update test.py

--- test.py
+++ test.py
@@ -18,7 +18,7 @@ st.title("👋 Welcome to Smart Parking Manamgement App v1.0")
 #Starting from the top
 st.markdown("# Dedicated for Smart City Management Framework™")
 st.markdown("Project realized by: Naga Srinivas Reddy & Naga Siva Reddy & Shravan Yadav")
-''''
+'''
 original_title = '<p style="color:Orange; font-size: 30px;"> Let get started...</p>'
 st.markdown(original_title, unsafe_allow_html=True)
 
