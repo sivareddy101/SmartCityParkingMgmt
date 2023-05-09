2110f62e628c02f75a8dc79d3d0d1aba5f938327
sivareddy101 <132719318+sivareddy101@users.noreply.github.com>
2023-05-09 15:15:19 -0400

    Update test.py

--- test.py
+++ test.py
@@ -147,4 +147,11 @@ plt.xlabel('Day')
 plt.ylabel('Number of Entries')
 st.pyplot(plt.show())
 
-
+st.write('''
+Here we are grouping the data by date and time and compute the total occupancy for each group
+-for finding the date and time with the highest total occupency form the dataset
+''')
+st.info("The following we are predicting the busiest date and time from the dataset")
+occupancy_by_date_time = dataset.groupby(['day', 'month', 'year', 'hour'])['occupancy'].sum()
+busiest_date_time = occupancy_by_date_time.idxmax()
+print("The busiest day and time is: {}-{}-{} {}:00".format(busiest_date_time[0], busiest_date_time[1], busiest_date_time[2], busiest_date_time[3]))
